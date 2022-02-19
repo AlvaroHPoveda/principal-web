c96a333d3096c24dcd62502de66146f80d79fd03
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-02-19 17:41:35 -0500

    menu hamburguesa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBurger} from "@fortawesome/free-solid-svg-icons";
+import "../styles/header.css";
+
 const Header = () => {
   return (
     <div className="Header">
       <header>
         <nav>
+          <input type="checkbox" id="menu" />
+          <label htmlFor="menu"><FontAwesomeIcon icon={faBurger} /></label>
           <ul>
             <li>
-              <Link to="/">Inicio</Link>
+              <Link to="/" className="link">Inicio</Link>
             </li>
             <li>
               {" "}
-              <Link to="/aboutus">Sobre nosotros</Link>
+              <Link to="/aboutus" className="link">Sobre nosotros</Link>
             </li>
             <li>
               {" "}
-              <Link to="/clothing">Boutique</Link>
+              <Link to="/clothing" className="link">Boutique</Link>
             </li>
             <li>
               {" "}
-              <Link to="/jewerly">Joyería</Link>
+              <Link to="/jewerly" className="link">Joyería</Link>
             </li>
             <li>
               {" "}
-              <Link to="/tecnology">Tecnología</Link>
+              <Link to="/tecnology" className="link">Tecnología</Link>
             </li>
           </ul>
-        </nav>
+        </nav>       
       </header>
     </div>
   );
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
+import '../styles/home.css'
 
 const Home = () => {
   return (
     <div>
-      <h1>Inicio</h1>
-      <p>esto lo escribio Gaston</p>
+      <h1>Inicio</h1>    
     </div>
   );
 };
