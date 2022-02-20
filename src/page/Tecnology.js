2b52451b55f6dea704e91ceb9d178f8c6d9525aa
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-02-20 09:45:34 -0500

    tecnologia

--- src/page/Tecnology.js
+++ src/page/Tecnology.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { getAllUsers } from "../utils/users";
 
 const Tecnology = () => {
+  const users = getAllUsers();
+  console.log(users)
   return (
     <div>
       <h1>Tecnologia</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <h2>
+              <Link to={"/tecnology/" + user.id}>{user.name}</Link>
+            </h2>
+          </li>
+        ))}
+      </ul>      
     </div>
   );
 };
