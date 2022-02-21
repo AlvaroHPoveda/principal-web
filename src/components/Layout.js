7ab13508bf93e5c33ef5f8bae55358c3ea9b93b5
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-02-21 01:03:25 -0500

    burguer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBurger, faXmarkCircle } from "@fortawesome/free-solid-svg-icons";
@@ -8,50 +8,45 @@ import { Footer } from ".";
 const Layout = () => {
   return (
     <main>
-      <nav>
-        <input className="inputLayout" type="checkbox" id="menu" />
-        <label htmlFor="menu">
-          <FontAwesomeIcon icon={faBurger} />
-        </label>
-        <ul className="ulLayout">
-          <li>
-            <Link to="/" className="link">
-              Inicio
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/aboutus" className="link">
-              Sobre nosotros
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/clothing" className="link">
-              Boutique
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/jewerly" className="link">
-              Joyería
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <Link to="/tecnology" className="link">
-              Tecnología
-            </Link>
-          </li>
-          <li>
-            {" "}
-            <input className="inputLayout" type="checkbox" id="menu" />
-            <label htmlFor="menu">
-              <FontAwesomeIcon icon={faXmarkCircle} />
-            </label>
-          </li>
-        </ul>
-      </nav>
+      <header className="header">
+        <input className="checkbox__hack" type="checkbox" id="checkbox__hack" />
+        <label
+          htmlFor="checkbox__hack"
+          className="checkbox-hack__label"
+        ></label>
+        <nav className="nav--top">
+          <ul className="menu-lateral nav--top__list">
+            <li className="menu-lateral__item">
+              <Link to="/" className="link">
+                Inicio
+              </Link>
+            </li>
+            <li className="menu-lateral__item">
+              <Link to="/aboutus" className="link">
+                Sobre nosotros
+              </Link>
+            </li>
+            <div>             
+              <strong>Proyectos</strong> 
+              <li className="menu-lateral__item">
+                <Link to="/clothing" className="link">
+                  Boutique
+                </Link>
+              </li>
+              <li className="menu-lateral__item">
+                <Link to="/jewerly" className="link">
+                  Joyería
+                </Link>
+              </li>
+              <li className="menu-lateral__item">
+                <Link to="/tecnology" className="link">
+                  Tecnología
+                </Link>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </header>
       <section>
         <Outlet />
         <Footer />
